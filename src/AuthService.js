02ba8bf0561c277b7c9d76e63a764e0c3680e2b7
Yatheesh D C <yatheesh2002dc@gmail.com git config --global user.name Your>
2024-07-07 15:52:04 +0530

    fourth

--- src/AuthService.js
+++ src/AuthService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth/';
+const API_URL = 'https://back-smqd.onrender.com/api/auth/';
 
 const register = (email, password) => {
   return axios.post(API_URL + 'register', { email, password });
